18b5b1202a5d38720e4ce35f75c8ed8dd7c395dc
Thomas Williamson <twilliamson.valdes@gmail.com>
2024-10-09 22:51:42 -0300

    Agrega paquetes y configuraciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:random_user/app_theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
+import 'app_theme.dart';
+import 'blocs/random_user_bloc.dart';
+import 'core/observer/app_bloc_observer.dart';
 import 'pages/landing_page.dart';
 
 void main() {
+  sqfliteFfiInit(); //Necesario para base sqlite
+  Bloc.observer = AppBlocObserver(); //Logger para eventos de bloc
   runApp(const MyApp());
 }
 
@@ -15,7 +21,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Random User',
       theme: AppTheme.theme,
-      home: const LandingPage(),
+      home: BlocProvider(
+        create: (context) => RandomUserBloc(),
+        child: const LandingPage(),
+      ),
     );
   }
 }
